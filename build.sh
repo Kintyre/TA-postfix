8d2b2f884d998250acb73410164f5624428f0ff6
Lowell Alleman <lowell@kintyre.co>
2018-04-25 10:03:42 -0400

    Fixup Travis build and release
    - Exclude more hidden (dot) file from archive.
    - Setup travis.yml to do all the build steps directly.  This more cleanly
      splits out install vs (test) scripts
    - Make 'build.sh' work locally only.

--- build.sh
+++ build.sh
@@ -1,43 +1,35 @@
 #!/bin/bash
-git_wd=$(pwd)
 
 function RUN(){
     echo "RUN " "$@"
     "$@"
 }
 
-# Run in a fresh virtual env; shouldn't be needed when running in Travis CI 
-if [[ -z $VIRTUAL_ENV ]]
+# Run in a fresh virtual env if 'splunk-appinspect' isn't available
+BIN=$(which splunk-appinspect)
+if [[ -z $BIN ]]
     then
     if ! [[ -d "venv" ]]
         then
         RUN virtualenv venv || exit 1
+
         # Upgrade to pip10  due to:  TLSV1_ALERT_PROTOCOL_VERSION
         RUN curl -s https://bootstrap.pypa.io/get-pip.py -o get-pip.py
         RUN python get-pip.py
+
+        RUN curl -sL http://dev.splunk.com/goto/appinspectdownload -o splunk-appinspect.tar.gz || exit 1
+        #Todo:  Verify the MD5/SHA (download broken when I just checked)
+        pkg=splunk-appinspect.tar.gz
+
+        RUN pip install "$pkg"
     fi
     echo "Activating virtualenvironment"
     source venv/bin/activate || exit
-
 fi
+RUN splunk-appinspect list version || exit
 
-RUN curl -sL http://dev.splunk.com/goto/appinspectdownload -o splunk-appinspect.tar.gz || exit 1
-#Todo:  Verify the MD5/SHA (download broken when I just checked)
-pkg=splunk-appinspect.tar.gz
 
-# Testing with a local pre-downloaded package
-#pkg=splunk-appinspect-1.5.4.145.tar.gz 
-
-# No need to extract manually....
-##appinspect_dir=$(mktemp -d)
-##echo "Using temp dir $appinspect_dir"
-##RUN tar -xzvf splunk-appinspect.tar.gz -C "$appinspect_dir" || exit 1
-# Should be only one new subdirectory from download
-##pkg="$appinspect_dir"/*
-
-RUN pip install "$pkg"
 RUN ./release.sh
 pkg=$(cat latest_release)
 
-RUN splunk-appinspect list version
 RUN splunk-appinspect inspect "$pkg" --mode precert --included-tags cloud
